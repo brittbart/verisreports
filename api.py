48bd65df4760d33b332f20117edf5e3f4a2d8a9a
brittbart <brittpattdenver@gmail.com>
2026-04-20 15:45:41 -0600

    Force redeploy

--- api.py
+++ api.py
@@ -102,4 +102,4 @@ def stats():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)# Railway deployment Mon Apr 20 15:45:41 MDT 2026
