600200ea9b8cc188e9385117d07e7b889e999ea1
brittbart <brittpattdenver@gmail.com>
2026-05-04 20:36:20 -0600

    Restore DB password fallback with new password

--- api.py
+++ api.py
@@ -30,7 +30,7 @@ def get_db():
         **(dict(dsn=os.environ['DATABASE_URL']) if os.environ.get('DATABASE_URL') else dict(
             dbname=os.environ.get('DB_NAME', 'railway'),
             user=os.environ.get('DB_USER', 'postgres'),
-            password=os.environ.get('DB_PASSWORD', 'Kx9mPqR7nWjL2vTsYdF4bHcE6uZaGpNe'),
+            password=os.environ.get('DB_PASSWORD', 'd3aclhdlo3wynv76IMqR9GOx44PHp3eo'),
             host=os.environ.get('DB_HOST', 'shinkansen.proxy.rlwy.net'),
             port=os.environ.get('DB_PORT', '35370')
         ))
